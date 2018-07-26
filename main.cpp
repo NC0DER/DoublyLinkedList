3e400e1739ed9b2a84d3df2165a0d13bec2407ab
Unknown <dynarithm@gmail.com>
2018-07-26 22:34:56 +0300

    Completed append method, added first version of print method
    and added some tests in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
-#include <iostream>
-#include <memory>
 #include "LinkedList.h"
 
-int main(){ //Temporary Test Main will be split from the implementation file in the future
+int main() { //Temporary Test Main will be split from the implementation file in the future
+    DLL::LinkedList <int> list;
+    list.append(1);
+    list.append(2);
+    list.append(3);
+
+    list.print();
     return 0;
 }
